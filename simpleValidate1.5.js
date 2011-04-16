5f1f9e5abb9caa4d167fa7efacb5ae6416f2f793
Ajaxman <sjavierlopez@gmail.com>
2011-04-16 18:48:00 -0500

    SimpleValidate Base created

--- simpleValidate1.5.js
+++ simpleValidate1.5.js
@@ -36,38 +36,36 @@
 
                //Trigger of Plugin
                $this.submit(function(form) {
-                  // setting value of return
-                  var _return_ = true;
+                   // setting value of return
+                    var _return_ = true;
 
-                  $('#' + $idform + ' [title^=' + options.field + ']').each(function(){
-
-                    var $input = $(this);
-                    var $validator = $input.attr('title');
+                    $('#' + $idform + ' [title^=' + options.field + ']').each(function(){
+                        var $input = $(this);
+                        var $validator = $input.attr('title');
                         _return_   = $this.__init__($validator,$input);
                         if(_return_ === false){
-                        this.focus();
-                        return false;
-                    }
+                            this.focus();
+                            return false;
+                        }
+                    });//End of filter check input valids
 
-                    if (typeof callback == 'function') { // make sure the callback is a function
-
+                    if(typeof callback == 'function') { // make sure the callback is a function
                         callback.call(this); // brings the scope to the callback
                         form.preventDefault();
                         return false;
-
                     }else{
-
                         return true;
-
                     }
-
-                  });//End of filter check input valids
-                  
                });//End of trigger
 
 
-               $this.__init__ = function(){
-
+               $this.__init__ = function(validator,field){
+                   //Set options
+                   var filter = /\[(.*)\]/;
+                   var validator = filter.exec(validator);
+                   var value = field.value;
+                   var _return_ = true;//Force return true;
+                   
                }            
 
            }
