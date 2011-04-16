aab716397093d8828c767067154218d034a787ef
Ajaxman <sjavierlopez@gmail.com>
2011-04-16 16:09:57 -0500

    Prueba de options

--- simpleValidate1.5.js
+++ simpleValidate1.5.js
@@ -1,6 +1,27 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*!
+ * simpleValidate plugin Jquery
+ *
+ *
+ * Copyright 2011, Javier López López(aka Ajaxman)
+ * Licensed under GPL Version 3 license.
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * @version 1.5 beta
+ *
  */
+(function($){
+        $.fn.extend({
 
+           simpleValidate: function(options,callback) {
 
+               var defaults = {
+                    text: Array("El campo"," es obligatorio"),
+                    mail: "Ingrese un email valido",
+                    zip: "Ingrese un codigo postal valido",
+                    min: Array(5,"Ingrese al menos ", "caracteres"),
+                    max: Array(10,"Ingrese maximo", "caracteres"),
+               }
+           }
+        });
+
+})( jQuery );
